7ee5b4e3ceb1eb412eecdc707836b076752fc3d0
Ambrosevich Roman <etoxroman@gmail.com>
2024-12-18 21:45:47 +0300

    donedd

--- undo_redo_manager.cpp
+++ undo_redo_manager.cpp
@@ -40,6 +40,9 @@ void UndoRedoManager::createSnapshot() {
 }
 
 void UndoRedoManager::undo() {
+    if (undoStack.size() <= 1) {
+        throw runtime_error("No more actions to undo.");
+    }
 
     redoStack.push_back(undoStack.back());
     undoStack.pop_back();
@@ -48,6 +51,9 @@ void UndoRedoManager::undo() {
 }
 
 void UndoRedoManager::redo() {
+    if (redoStack.empty()) {
+        throw runtime_error("No more actions to redo.");
+    }
 
     undoStack.push_back(redoStack.back());
     redoStack.pop_back();
